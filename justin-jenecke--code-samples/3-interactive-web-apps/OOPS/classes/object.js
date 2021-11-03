903d04fa22c7ed882a73bb935abf46958d930d8b
Justin <justinjenecke@gmail.com>
2021-11-03 21:12:19 +0200

    makes demonstration changes to classes example, this example, as well as structuring and interfacing code

--- justin-jenecke--code-samples/3-interactive-web-apps/OOPS/classes/object.js
+++ justin-jenecke--code-samples/3-interactive-web-apps/OOPS/classes/object.js
@@ -1,4 +1,4 @@
-/*
+
 // example of tv as class
 class Car {
 
@@ -10,19 +10,56 @@ class Car {
         this._manufacturer = manufacturer;
         this._year = year;
         this._cost = cost;
-    
     }
+
+    getModel() {
+        return this._model;
+      }
+
 }//end of class
 
 
 // Objects of the Car class
 
-const showRoom = [];
+const person = {
+    name : 'justin',
+    job : 'coach'
+}
 
 
-const todoList = document.getElementById('todoBody');
-const create = document.getElementById('create');
-*/
+
+const myCar = new Car ('Fiesta', 'Ford', 2010, 100);
+const car2 = new Car ('X16', 'Ford', 2018, 100);
+const car3 = new Car ('Focus', 'Ford', 2010, 100);
+const car4 = new Car ('Golf', 'VW', 2020, 100);
+
+
+const showRoom = [myCar, car2, car3, car4];
+
+for (let index = 0; index < showRoom.length; index++) {
+    
+    console.log(showRoom[index]);
+    
+}
+
+const entryPoint = document.getElementById('elem');
+const newElem = document.createElement('div');
+
+showRoom.forEach(car => {
+
+    entryPoint.innerHTML +=
+
+        `<div id="${car._model}"> 
+            <span class ="carDiv">${car._model}</span>
+            <span class ="carDiv">${car._year}</span>
+            <span class ="carDiv">${car._cost}</span>
+            <span class ="carDiv">${car.manufacturerel}</span>
+            
+        </div>`
+
+});
+
+
 /*
 create.addEventListener('click', () => {
 
---
+++ justin-jenecke--code-samples/3-interactive-web-apps/Structuring&Interfacing/notes.md
@@ -0,0 +1,9 @@
+##  JSON:
+  
+JSON data is in Key : Value pairs
+Key : Value pairs are separated by commas (,)
+
+
+    Objects should be encapsulated by curly braces {}
+    Arrays should be encapsulated by square brackets []
+
