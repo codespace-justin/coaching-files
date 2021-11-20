92a44da730f3e77db0194d70d8af182856666baf
Justin <justinjenecke@gmail.com>
2021-11-20 19:38:58 +0200

    creates array example code

---
+++ justin-jenecke--code-samples/5-backend-programming/b-EnumeratingPHP/arrays.php
@@ -0,0 +1,9 @@
+<?php
+
+# An array is basically a list of variables where each variable can be referred to based on its position in the list (aka Index)
+
+# Declare Array #1
+$myArray1 = array();
+
+# Declare Array #2
+$myArray2 = [];
