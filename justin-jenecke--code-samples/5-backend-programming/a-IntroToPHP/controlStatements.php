71163e4c60bf936f00493ccb90f2f523f5321e39
Justin <justinjenecke@gmail.com>
2021-11-26 09:10:41 +0200

    creates basic php example code

--- justin-jenecke--code-samples/5-backend-programming/a-IntroToPHP/controlStatements.php
+++ justin-jenecke--code-samples/5-backend-programming/a-IntroToPHP/controlStatements.php
@@ -14,6 +14,7 @@ Basic Control Statements:
 
 */
 
+echo "something";
 
 # Basic Conditionals:  >, <, >=, <= and ==
 
--- justin-jenecke--code-samples/5-backend-programming/a-IntroToPHP/intro.php
+++ justin-jenecke--code-samples/5-backend-programming/a-IntroToPHP/intro.php
@@ -2,4 +2,9 @@
 
     # how you print strings in PHP
     echo 'Hello World';
+    echo 'Hello World';
+    echo 'Hello World';
 
+    echo "algjewj";
+
+    
