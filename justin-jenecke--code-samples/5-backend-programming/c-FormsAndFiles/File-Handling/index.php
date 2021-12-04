9c9052e23ae2c1a0d6376735ab6b328a81ca4f27
Justin Scott Jenecke <93656319+codespace-justin@users.noreply.github.com>
2021-12-04 19:54:35 +0200

    adds code example of uploading multiple files

--- justin-jenecke--code-samples/5-backend-programming/c-FormsAndFiles/File-Handling/index.php
+++ justin-jenecke--code-samples/5-backend-programming/c-FormsAndFiles/File-Handling/index.php
@@ -10,12 +10,24 @@
 
     <body>
 
+        <h1>Single File</h1>
+
         <form action="upload.php" method="post" enctype="multipart/form-data">
             <label for="fileToUpload">Select images to upload:</label><br>
             <input type="file" name="fileToUpload" id="fileToUpload"><br>
             <input type="submit" name="submit">
         </form>
 
+        <br>
+
+        <h1>Multiple Files</h1>
+
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+            <label for="fileToUpload">Select images to upload:</label>
+            <input type="file" name="fileToUpload" id="fileToUpload">
+            <input type="file" name="fileToUpload2" id="fileToUpload2">
+            <input type="submit" name="submit">
+        </form>
     </body>
 
 </html>
--- justin-jenecke--code-samples/5-backend-programming/c-FormsAndFiles/File-Handling/upload.php
+++ justin-jenecke--code-samples/5-backend-programming/c-FormsAndFiles/File-Handling/upload.php
@@ -1,11 +1,34 @@
 
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
 
-    $target_dir = "assets/images/";
-    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-       
-    move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
+    <body>
 
-?>
+    <!-- Script to handle saving file -->
+        <?php
 
-<img src="<?echo $target_file;?>" alt="">
+            /*$target_dir = "assets/images/";
+            $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+            
+            move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
+        */
+            $target_dir = "assets/images/";
+
+            foreach ($_FILES as $file){
+                $target_file = $target_dir . basename($file["name"]);
+                move_uploaded_file($file["tmp_name"], $target_file);
+            }
+        
+        ?>
+
+        <!-- <img src="<?#echo $target_file;?>" alt="" height="200" width="200"> -->
+
+    </body>
+</html>
+
